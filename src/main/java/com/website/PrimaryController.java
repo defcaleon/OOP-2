8a93151d462157a4d0716b92c43c4574400ff705
Nikita Parmon <nikitka.parmon@mail.ru>
2021-03-16 18:25:58 +0300

    init commit

---
+++ src/main/java/com/website/PrimaryController.java
@@ -0,0 +1,8 @@
+package com.website;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.website {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.website to javafx.fxml;
+    exports com.website;
+}
